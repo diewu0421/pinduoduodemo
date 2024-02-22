c642e553fbb71844257e53ede27d058258de8ac2
zenglw <zenglw@ky-tech.com.cn>
2024-02-22 21:34:23 +0800

    Notify

--- app/src/main/java/com/example/accessbilityservicedemo/NotifyService.kt
+++ app/src/main/java/com/example/accessbilityservicedemo/NotifyService.kt
@@ -1,9 +1,16 @@
 package com.example.accessbilityservicedemo
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 
 /**
  * bytedance
@@ -13,9 +20,33 @@ import android.util.Log
  */
  class NotifyService : NotificationListenerService()  {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         super.onNotificationPosted(sbn)
-        Log.e(TAG, "onNotificationPosted: ${sbn?.packageName}， content:${sbn?.notification?.extras?.getString("android.text")}", )
+        val text = sbn?.notification?.extras?.getString("android.text")
+        val channelId = sbn?.notification?.channelId
+        if (channelId == "my_channel") {
+            return
+        }
+        Log.e(TAG, "onNotificationPosted: ${sbn?.packageName}， content:${text}", )
+        // 创建通知渠道
+        val channel = NotificationChannel("my_channel",
+            "My Channel", NotificationManager.IMPORTANCE_HIGH)
+        channel.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+
+// 获取通知管理器
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
+// 创建通知
+        val notification = NotificationCompat.Builder(this, channel.id)
+            .setContentTitle("This is a notification")
+            .setContentText(text)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .build()
+
+// 发送通知
+        notificationManager.notify(1, notification)
         
     }
 
